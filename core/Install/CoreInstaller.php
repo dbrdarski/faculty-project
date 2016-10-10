c389210a4365283791c70f2936a2560f9a5cd4be
{Dane Brdarski} <{dane.brdarski@gmail.com}>
2016-10-10 17:33:46 +0200

    Improved the schema

--- core/Install/CoreInstaller.php
+++ core/Install/CoreInstaller.php
@@ -18,13 +18,29 @@ class CoreInstaller{
 				$table->string('lastName');
 				$table->string('gender');
 				$table->string('type');
+				$table->boolean('active');
 				$table->timestamps();
 			})
 		)
 		->add(new Table('courses', 
 			function ($table){
 				$table->increments('id');
-				$table->string('courseName');
+				$table->integer('owner');
+				$table->foreign('owner')->references('id')->on('users');
+				$table->string('name');
+				$table->boolean('active');
+				$table->timestamps();
+			})
+		)
+		->add(new Table('subscriptions', 
+			function ($table){
+				$table->increments('id');
+				$table->integer('userId');
+				$table->foreign('userId')->references('id')->on('users');
+				$table->integer('courseId');
+				$table->foreign('courseId')->references('id')->on('courses');
+				$table->integer('grade');
+				$table->integer('rating');
 				$table->timestamps();
 			})
 		);
